39421157ad47d67ab6abfbcbe87a7b16b7bc46af
Vitorsilva08 <vitorsilvalimadasilvavv@gmail.com>
2026-04-23 16:02:13 -0300

    primeiro exercicio

---
+++ handson/script.js
@@ -0,0 +1,12 @@
+// Exercicio de condicional 
+nivelAcesso= 1
+if (nivelAcesso== 2){
+    console.log("acesso total");
+}
+else if (nivelAcesso== 3){
+    console.log("acesso parcial");
+}
+else {
+    console.log("acesso negado");
+}
+
